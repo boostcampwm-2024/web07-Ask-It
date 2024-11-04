491fa19d92da45fb2401a7f6d367f014b8500d03
cjeongmin <cjeongmin27@gmail.com>
2024-11-05 00:20:24 +0900

    chore: add @tanstack/react-query-devtools to package.json and pnpm-lock.yaml; integrate with QueryClient in App component

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
