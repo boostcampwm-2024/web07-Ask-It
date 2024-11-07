add87cab8e2c28a8a46327b54970555df8350990
cjeongmin <cjeongmin27@gmail.com>
2024-11-08 00:58:16 +0900

    feat: add keyboard event handling to close modal on Escape key

--- apps/client/src/features/modal/components/Background.tsx
+++ apps/client/src/features/modal/components/Background.tsx
@@ -1,15 +1,31 @@
-import { MouseEvent, PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 
-function Background({
-  children,
-  onClick,
-}: PropsWithChildren<{
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
-}>) {
+import { useModalContext } from '@/features/modal';
+
+function Background({ children }: PropsWithChildren) {
+  const { closeModal } = useModalContext();
+
+  useEffect(() => {
+    const handleEsc = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') closeModal();
+    };
+
+    document.addEventListener('keydown', handleEsc);
+
+    return () => {
+      document.removeEventListener('keydown', handleEsc);
+    };
+  }, [closeModal]);
+
   return (
     <button
       type='button'
-      onClick={onClick}
+      onClick={closeModal}
+      onKeyDown={(e) => {
+        if (e.key === ' ') {
+          e.preventDefault();
+        }
+      }}
       className='fixed left-0 top-0 z-10 flex h-dvh w-dvw cursor-auto items-center justify-center bg-[#808080]/20 backdrop-blur-sm'
     >
       {children}
