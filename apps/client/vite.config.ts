7afd9ca6dc2a093ca60934bac86eff25250213ec
cjeongmin <cjeongmin27@gmail.com>
2024-11-10 00:34:54 +0900

    feat: update Vite configuration to set base URL based on environment mode

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: 'https://ask-it-static.kr.object.ncloudstorage.com/dist',
-  plugins: [TanStackRouterVite(), react()],
-  resolve: {
-    alias: {
-      '@': '/src',
+export default defineConfig(({ mode }) => {
+  return {
+    base:
+      mode === 'production'
+        ? 'https://ask-it-static.kr.object.ncloudstorage.com/dist'
+        : '/',
+    plugins: [TanStackRouterVite(), react()],
+    resolve: {
+      alias: {
+        '@': '/src',
+      },
     },
-  },
+  };
 });
