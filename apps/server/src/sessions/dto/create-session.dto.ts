cdac9eb8319022b052935e269d119e87b606e834
shl0501 <shl050105010501@gmail.com>
2024-11-09 12:03:11 +0900

    feat: refactor swagger

--- apps/server/src/sessions/dto/create-session.dto.ts
+++ apps/server/src/sessions/dto/create-session.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateSessionDto {
@@ -7,6 +8,11 @@ export class CreateSessionDto {
   //로그인 기능 구현 시 @IsNotEmpty() 추가
   create_user_id: number;
 
+  @ApiProperty({
+    example: 'temporary title',
+    description: 'session의 title',
+    required: true,
+  })
   @IsNotEmpty({ message: 'title이 입력되어야 합니다' })
   @IsString()
   title: string;
