58e25e1574b9d066720c4ecbdfd72da0301a742c
cjeongmin <cjeongmin27@gmail.com>
2024-11-09 19:59:09 +0900

    feat: set base URL for Vite configuration

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -4,6 +4,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://ask-it-static.kr.object.ncloudstorage.com/dist',
   plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
