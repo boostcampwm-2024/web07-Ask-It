4095eca815cfdfdc1fb3ad1ff68c0f6c8d63154d
cjeongmin <cjeongmin27@gmail.com>
2024-11-14 21:20:09 +0900

    feat: enhance sign-in form logic with improved error handling and validation

--- apps/client/src/features/auth/auth.hook.ts
+++ apps/client/src/features/auth/auth.hook.ts
@@ -1,18 +1,54 @@
+import { isAxiosError } from 'axios';
 import { useState } from 'react';
 
+import { login } from '@/features/auth/auth.api';
+import { useAuthStore } from '@/features/auth/auth.store';
+import { ValidationStatusWithMessage } from '@/shared';
+
 export function useSignInForm() {
+  const { setAccessToken } = useAuthStore();
+
   const [email, setEmail] = useState('');
 
   const [password, setPassword] = useState('');
 
-  const [isLoginFailed, setIsLoginFailed] = useState(false);
+  const isLoginEnabled = email.length > 0 && password.length > 7;
+
+  const [loginFailed, setLoginFailed] = useState<ValidationStatusWithMessage>({
+    status: 'INITIAL',
+  });
+
+  const handleLogin = async () => {
+    try {
+      const response = await login({ email, password });
+
+      if (response.type === 'success') {
+        setAccessToken(response.data.accessToken);
+      }
+    } catch (e) {
+      if (isAxiosError(e) && e.response && 'error' in e.response.data) {
+        if (e.response.status === 400) {
+          setLoginFailed({
+            status: 'INVALID',
+            message: e.response.data.error.messages.shift(),
+          });
+        } else if (e.response.status === 401) {
+          setLoginFailed({
+            status: 'INVALID',
+            message: e.response.data.error.message,
+          });
+        }
+      }
+    }
+  };
 
   return {
     email,
     setEmail,
     password,
     setPassword,
-    isLoginFailed,
-    setIsLoginFailed,
+    isLoginEnabled,
+    loginFailed,
+    handleLogin,
   };
 }
