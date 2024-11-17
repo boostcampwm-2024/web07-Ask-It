49ed9e86d3f59233723558d5c561c40ce338bbee
cjeongmin <cjeongmin27@gmail.com>
2024-11-17 22:23:51 +0900

    feat: enhance toast store to generate unique IDs for toasts

--- apps/client/src/features/toast/toast.store.ts
+++ apps/client/src/features/toast/toast.store.ts
@@ -4,24 +4,35 @@ import { Toast } from '@/features/toast/toast.type';
 
 interface ToastStore {
   toasts: Toast[];
-  addToast: (toast: Toast) => void;
+  addToast: (toast: Omit<Toast, 'isActive' | 'id'>) => void;
 }
 
 export const useToastStore = create<ToastStore>((set) => ({
   toasts: [],
   addToast: (toast) => {
-    set((state) => ({ toasts: [...state.toasts, toast] }));
+    const id = Math.random().toString(36).substring(2, 9);
+
+    set((state) => ({
+      toasts: [
+        ...state.toasts,
+        {
+          ...toast,
+          isActive: true,
+          id,
+        },
+      ],
+    }));
 
     setTimeout(() => {
       set((state) => ({
         toasts: state.toasts.map((t) =>
-          t.id === toast.id ? { ...t, isActive: false } : t,
+          t.id === id ? { ...t, isActive: false } : t,
         ),
       }));
 
       setTimeout(() => {
         set((state) => ({
-          toasts: state.toasts.filter((t) => t.id !== toast.id),
+          toasts: state.toasts.filter((t) => t.id !== id),
         }));
       }, 300);
     }, toast.duration);
