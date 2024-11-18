522ed8953bed1737926c9765b9c10737ec834f59
cjeongmin <cjeongmin27@gmail.com>
2024-11-18 14:11:23 +0900

    feat: add authentication check in my route before loading the page

--- apps/client/src/routes/my.tsx
+++ apps/client/src/routes/my.tsx
@@ -1,7 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
+import { useAuthStore } from '@/features/auth';
 import { MyPage } from '@/pages';
 
 export const Route = createFileRoute('/my')({
   component: MyPage,
+  beforeLoad: async () => {
+    const { accessToken } = useAuthStore.getState();
+    if (!accessToken) {
+      // eslint-disable-next-line @typescript-eslint/no-throw-literal
+      throw redirect({ to: '/' });
+    }
+  },
 });
