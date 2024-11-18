938700cfdc6214affb5621ae5ed3bfd805c42afe
cjeongmin <cjeongmin27@gmail.com>
2024-11-18 14:11:55 +0900

    feat: fetch session token in QnAPage and log sessionId in route before loading

--- apps/client/src/pages/QnAPage.tsx
+++ apps/client/src/pages/QnAPage.tsx
@@ -1,7 +1,9 @@
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from '@tanstack/react-router';
 
 import { ChattingList } from '@/components';
 import QuestionContent from '@/components/qna/QuestionContent';
+import { getSessionToken } from '@/features/session';
 import { QnAContextProvider } from '@/features/session/qna';
 
 function QnAPage() {
@@ -11,7 +13,12 @@ function QnAPage() {
     strict: true,
   });
 
-  console.log(sessionId);
+  const { data: token } = useQuery({
+    queryKey: ['/sessions-auth', sessionId],
+    queryFn: () => getSessionToken(sessionId),
+  });
+
+  console.log(token);
 
   return (
     <div className='flex h-full w-full items-center justify-center gap-4 px-4 py-4 md:max-w-[1194px]'>
--- apps/client/src/routes/session.$sessionId.tsx
+++ apps/client/src/routes/session.$sessionId.tsx
@@ -4,4 +4,11 @@ import { QnAPage } from '@/pages';
 
 export const Route = createFileRoute('/session/$sessionId')({
   component: QnAPage,
+  beforeLoad: async ({ params: { sessionId } }) => {
+    /**
+     * TODO
+     * 여기에서 데이터를 가져오고 전역상태로 데이터 이전
+     */
+    console.log(sessionId);
+  },
 });
