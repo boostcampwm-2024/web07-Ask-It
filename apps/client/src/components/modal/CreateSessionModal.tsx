e064df2dc52e7506a3bbd210194807536d2be75a
Choi Jeongmin <cjeongmin27@gmail.com>
2024-11-26 15:59:18 +0900

    fix: update session name length validation

--- apps/client/src/components/modal/CreateSessionModal.tsx
+++ apps/client/src/components/modal/CreateSessionModal.tsx
@@ -36,7 +36,7 @@ function CreateSessionModal() {
   });
 
   const enableCreateSession =
-    sessionName.trim().length > 0 && sessionName.trim().length <= 2;
+    sessionName.trim().length > 3 && sessionName.trim().length <= 20;
 
   const handleCreateSession = () => {
     if (!enableCreateSession || isPending) return;
